328d2489d3d7a269dca0bc1f131d82ab1bb344b8
Muhamad Rafiki <rafikimuhammad@gmail.com>
2023-10-27 15:13:00 -0500

    create css file

---
+++ scripts/main.js
@@ -0,0 +1,16 @@
+
+
+let originalRoomCost = 0;
+let discountAmounta = 0;
+let discountedRoomCost = 0;
+let taxAmount = 0;
+let totalCost = 0;
+const TAX_RATE = 0.12;
+
+
+function getRate(roomType,checkedInDate){
+    let rate = 150;
+}
+function calculate(night,roomType,childrenQuality,discountType){
+
+}
